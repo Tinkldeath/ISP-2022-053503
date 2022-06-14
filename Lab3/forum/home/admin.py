ce147fd57107c0d14e6b27440a4049b76ad5306d
Dmitriy <dropdeadit@gmail.com>
2022-06-14 15:46:00 +0300

    Админ-панель поприятнее

--- Lab3/forum/home/admin.py
+++ Lab3/forum/home/admin.py
@@ -1,8 +1,23 @@
 from django.contrib import admin
 from .models import Topic, Author, Tred, Comment, Reply
 
-admin.site.register(Topic)
-admin.site.register(Author)
-admin.site.register(Tred)
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ('name', 'role', 'user', 'rating')
+    search_fields = ('name', 'role')
+    list_editable = ('role', 'rating')
+
+class TopicAdmin(admin.ModelAdmin):
+    list_display = ('title', 'approved')
+    search_fields = ('title', 'description')
+    list_editable = ('approved',)
+
+class TredAdmin(admin.ModelAdmin):
+    list_display = ('title', 'content', 'author', 'approved')
+    search_fields = ('title', 'content', 'author')
+    list_editable = ('approved',)
+
+admin.site.register(Topic, TopicAdmin)
+admin.site.register(Author, AuthorAdmin)
+admin.site.register(Tred, TredAdmin)
 admin.site.register(Comment)
 admin.site.register(Reply)
